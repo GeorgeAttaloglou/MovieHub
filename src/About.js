1c93820fef15db6fd47296fe95c458008959fc71
GeorgeAttaloglou <georgeeattaloglou@gmail.com>
2025-03-17 12:47:00 +0200

    Add some styling to about page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,17 @@
 import React from "react";
+import "./About.css";
 
 function About() {
   return (
-    <div className="container text-center mt-5">
-      <h1>About Us</h1>
-      <p>This is the About page of our React application.</p>
+    <div className="about-container">
+
+      <div className="about-content">
+        <h1 className="about-title">About Us</h1>
+        <p className="about-text">
+          MovieHub is a platform that allows you to track, review, and discover movies and series tailored to your preferences.
+        </p>
+        <p className="about-text"></p>
+      </div>
     </div>
   );
 }
