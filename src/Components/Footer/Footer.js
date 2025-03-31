7df8dc7755b660841d1bc4753e893367dfbfb144
GeorgeAttaloglou <georgeeattaloglou@gmail.com>
2025-03-31 15:02:33 +0300

    Change order

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
 			<FooterContainer>
 				<Row>
 					<Column>
-						<Heading><FooterLink href="/about">About</FooterLink></Heading>
+						<Heading><FooterLink href="/profile">Profile</FooterLink></Heading>
 					</Column>
 					<Column>
 						<Heading><FooterLink href="/create-list">Make a list!</FooterLink></Heading>
@@ -33,7 +33,7 @@ const Footer = () => {
 						<Heading><FooterLink href="/browse-movies">Browse</FooterLink></Heading>
 					</Column>
 					<Column>
-						<Heading><FooterLink href="/profile">Profile</FooterLink></Heading>
+						<Heading><FooterLink href="/about">About</FooterLink></Heading>
 					</Column>
 				</Row>
 			</FooterContainer>
